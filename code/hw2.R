d09f851b6d5057f83c867ff5c0e11ff15992838e
emjonaitis <jonaitis@stat.wisc.edu>
2014-02-16 09:47:48 -0600

    Finished problem 1. Problem 2 still to go

--- code/hw2.R
+++ code/hw2.R
@@ -64,197 +64,27 @@ summary(glm.final)
 
 nhefs.final$propensity <- predict(glm.final, type="response")
 
-
-
 require(ggplot2)
 qplot(propensity,data=nhefs.final,group=qsmk,colour=qsmk,geom="density")
 
-#####################################################
-# Estimating IP weights
-# Data from NHEFS
-#####################################################
 
-# Estimation of ip weights via a logistic model
-fit <- glm(qsmk ~ as.factor(sex) + as.factor(race) + age + I(age^2) + 
-  as.factor(education.code) + smokeintensity + 
-  I(smokeintensity^2) + smokeyrs + I(smokeyrs^2) + 
-  as.factor(exercise) + as.factor(active) + wt71 + I(wt71^2), 
-           family = binomial(), data = nhefs0)
-summary(fit)
 
-p.qsmk.obs <- ifelse(nhefs0$qsmk == 0, 1 - predict(fit, type = "response"),
-                     predict(fit, type = "response"))
-nhefs0$w <- 1/p.qsmk.obs
-summary(nhefs0$w)
-sd(nhefs0$w)
+# (b) Stratification: classify individuals in 5 strata of approximately 
+# equal size, what is the estimated effect of smoking cessation on 
+# weight gain? How about 10 strata?
 
-# Estimates from a GEE
-#install.packages("geepack")
-require(geepack)
-gee.obj <- geeglm(wt82_71~qsmk, data = nhefs0, std.err = 'san.se',
-                  weights = w, id=seqn, corstr="independence")
-summary(gee.obj)
 
-library(multcomp)
-#install.packages("BSagri")
-library(BSagri)
- 
-glm.obj <- glm(wt82_71 ~ qsmk + cluster(seqn), data = nhefs0, weights = w)
-summary(glm.obj)
-comp<-glht(glm.obj)
-CIadj<-CIGLM(comp,method="Adj")
-CIadj
-vcov(glm.obj)
+ApplyQuantiles <- function(data,num) {
+  cut(data, breaks=c(quantile(data, probs = seq(0, 1, by = 1/num))), 
+      labels=c(1:num),include.lowest=TRUE)
+}
 
-# install.packages("sandwich")
-require(sandwich)
-beta <- coef(glm.obj)
-SE <-sqrt(diag(vcovHC(glm.obj, type="HC0"))) # robust standard errors
-lcl <- beta-1.96*SE 
-ucl <- beta+1.96*SE
-round(cbind(beta, lcl, ucl),1)[2,] 
- 
- 
-#####################################################
-# Estimating stabilized IP weights
-# Data from NHEFS
-#####################################################
+nhefs.final$rank5 <- with(nhefs.final,factor(ApplyQuantiles(propensity,5)))
+nhefs.final$rank10 <- with(nhefs.final,factor(ApplyQuantiles(propensity,10)))
 
-# estimation of denominator of ip weights
-denom.fit <- glm(qsmk ~ as.factor(sex) + as.factor(race) + age + I(age^2) + 
-  as.factor(education.code) + smokeintensity + 
-  I(smokeintensity^2) + smokeyrs + I(smokeyrs^2) + 
-  as.factor(exercise) + as.factor(active) + wt71 + I(wt71^2), 
-                 family = binomial(), data = nhefs0)
-summary(denom.fit)
 
-denom.p <- predict(denom.fit, type = "response")
+lm.rank5 <- lm(wt82_71~qsmk+rank5,data=nhefs.final)
+lm.rank10 <- lm(wt82_71~qsmk+rank10,data=nhefs.final)
 
-# estimation of numerator of ip weights
-numer.fit <- glm(qsmk~1, family = binomial(), data = nhefs0)
-summary(numer.fit)
-numer.p <- predict(numer.fit, type = "response")
-
-nhefs0$sw <- ifelse(nhefs0$qsmk == 0, ((1-numer.p)/(1-denom.p)),
-                    (numer.p/denom.p))
-
-summary(nhefs0$sw)
-
-library(multcomp)
-#install.packages("BSagri")
-library(BSagri)
-
-glm.obj <- glm(wt82_71~ qsmk + cluster(seqn), data = nhefs0, weights = sw)
-summary(glm.obj)
-comp<-glht(glm.obj)
-CIadj<-CIGLM(comp,method="Adj")
-CIadj
-
-beta <- coef(glm.obj)
-SE <-sqrt(diag(vcovHC(glm.obj, type="HC0"))) # robust standard errors
-lcl <- beta-1.96*SE 
-ucl <- beta+1.96*SE
-round(cbind(beta, lcl, ucl),1)[2,]
-
-#####################################################
-# Estimating the parameters of a marginal structural mean model
-# with a continuous treatment Data from NHEFS
-#####################################################
-
-# Analysis restricted to subjects reporting <=25 cig/day at baseline
-nhefs1 <- subset(nhefs0, smokeintensity <=25)
-dim(nhefs1)
-
-# estimation of denominator of ip weights
-den.fit.obj <- lm(smkintensity82_71 ~ as.factor(sex) + 
-  as.factor(race) + age + I(age^2) + 
-  as.factor(education.code) + smokeintensity + I(smokeintensity^2) +
-  smokeyrs + I(smokeyrs^2) + as.factor(exercise) + as.factor(active) + wt71  + 
-  I(wt71^2), data = nhefs1)
-p.den <- predict(den.fit.obj, type = "response")
-dens.den <- dnorm(nhefs1$smkintensity82_71, p.den, summary(den.fit.obj)$sigma)
-
-# estimation of numerator of ip weights
-num.fit.obj <- lm(smkintensity82_71 ~ 1, data = nhefs1)
-p.num <- predict(num.fit.obj, type = "response")
-dens.num <- dnorm(nhefs1$smkintensity82_71, p.num, summary(num.fit.obj)$sigma)
-
-# estimation of Stabilized weights
-nhefs1$sw.a = dens.num/dens.den
-summary(nhefs1$sw.a)
-
-gee.obj <- geeglm(wt82_71~smkintensity82_71 + I(smkintensity82_71^2), 
-                  data = nhefs1, std.err = 'san.se', weights = sw.a, id=seqn, 
-                  corstr="independence")
-summary(gee.obj)
-comp<-glht(gee.obj)
-CIadj<-CIGLM(comp,method="Adj")
-CIadj
-
-#####################################################
-# Estimating the parameters of a marginal structural logistic model
-# Data from NHEFS
-#####################################################
-
-# using the dataset nhefs0 
-# weights sw are also calculated 
-# Estimating the parameters of a marginal structural logistic model
-glm.obj <- glm(death ~ qsmk + cluster(seqn), data = nhefs0, 
-               weights = sw, family = binomial())
-summary(glm.obj)
-comp<-glht(glm.obj)
-CIadj<-CIGLM(comp,method="Adj")
-UnlogCI(CIadj)
-vcov(glm.obj)
-
-beta <- coef(glm.obj)
-SE <-sqrt(diag(vcovHC(glm.obj, type="HC0"))) # robust standard errors
-lcl <- beta-1.96*SE 
-ucl <- beta+1.96*SE
-round(exp(cbind(beta, lcl, ucl)),1)[2,]
-
-#####################################################
-# Assessing effect modification by sex using a marginal structural mean model
-# Data from NHEFS
-#####################################################
-
-table(nhefs0$sex)
-
-# estimation of denominator of ip weights
-denom.fit <- glm(qsmk ~ as.factor(sex) + as.factor(race) + age + I(age^2) + 
-  as.factor(education.code) + smokeintensity + 
-  I(smokeintensity^2) + smokeyrs + I(smokeyrs^2) + 
-  as.factor(exercise) + as.factor(active) + wt71 + I(wt71^2), 
-                 family = binomial(), data = nhefs0)
-summary(denom.fit)
-
-denom.p <- predict(denom.fit, type = "response")
-
-# estimation of numerator of ip weights
-numer.fit <- glm(qsmk~as.factor(sex), family = binomial(), data = nhefs0)
-summary(numer.fit)
-numer.p <- predict(numer.fit, type = "response")
-
-nhefs0$sw2 <- ifelse(nhefs0$qsmk == 0, ((1-numer.p)/(1-denom.p)),
-                     (numer.p/denom.p))
-
-summary(nhefs0$sw2)
-sd(nhefs0$sw2)
-
-# Estimating parameters of a marginal structural mean model
-glm.obj <- glm(wt82_71~as.factor(qsmk) + as.factor(sex) + 
-  as.factor(qsmk):as.factor(sex) + cluster(seqn), data = nhefs0, 
-               weights = sw2)
-summary(glm.obj)
-comp<-glht(glm.obj)
-CIadj<-CIGLM(comp,method="Adj")
-CIadj
-vcov(glm.obj)
-
-beta <- coef(glm.obj)
-SE <-sqrt(diag(vcovHC(glm.obj, type="HC0"))) # robust standard errors
-lcl <- beta-1.96*SE 
-ucl <- beta+1.96*SE
-p.value <- 2*(1-pnorm(abs(beta/SE)))
-round(cbind(beta, SE, lcl, ucl, p.value),1)[5,]
-
+summary(lm.rank5)
+summary(lm.rank10)
